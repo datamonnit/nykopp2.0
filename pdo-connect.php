be4ad63a3d12b595e27af8c69c92c25f77ec5e0c
urhov <urkk2@gmail.com>
2021-11-03 13:26:59 +0200

    juu

---
+++ pdo-connect.php
@@ -0,0 +1,13 @@
+<?php
+// Development
+$servername = "xampp";
+$db_username = "root";
+$db_password = "";
+$dbname = "test";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $db_username, $db_password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Error:" . $e->getMessage();
+}
