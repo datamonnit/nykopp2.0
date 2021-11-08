6d330c7dacfc7baefec14d3d681381c115c7b399
urhov <urkk2@gmail.com>
2021-11-08 13:31:54 +0200

    jees

--- backend/registerNewUser.php
+++ backend/registerNewUser.php
@@ -1,44 +1,46 @@
 <?php 
-session_start();
 
-
-$data = array();
-
-
-if (!isset($_POST['username']) || !isset($_POST('password'))){
-    $data = array(
-        'error' => 'post_dataa ei saatavilla!'
-    );
-    die();
+if (!isset($_POST['username']) || !isset($_POST['password'])){
+  $data = array(
+    'error' => 'POST-data ei saatavilla!'
+  );
+  die();
 }
 $username = $_POST['username'];
+$email = $_POST['emailUsers'];
 $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
 
 
 include_once 'pdo-connect.php';
  
-try{
-    $stmt = $conn->prepare("INSERT INTO username, (username, password) VALUES  (:username, :username);");
+try {
+    $stmt = $conn->prepare("INSERT INTO user (username, email, pwd) VALUES (:username, :pwd);");
     $stmt->bindParam(':username', $username);
-    $stmt->bindParam(':password', $password);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':pwd', $password);
     if($stmt->execute() == false){
-    echo 'tallennus epäonnistui';
-    } else {
-    echo 'tallennettu';
+        $data = array(
+            'error' => 'tapahtui joku virhe tallennuksessa!'
+        );
+ }  else {
+    $data = array(
+    'success' => 'uusi käyttäjä on tallennettu!'
+    );
 }
-
+    }
 catch (PDOException $e) {
 if (strpos($e->getMessage(), ' 1062 Duplicate entry')){
-    echo 'käyttäjä on jo olemassa';
-    } 
     $data = array(
-        'error' => 'käyttäjä on jo olemassa!';
+       'error' => 'käyttäjä on jo olemassa!'
+    );
+} 
+    $data = array(
+        'error' => 'tuli virhe käyttäjän tallentamisessa!'
     );  
 
 } 
     
-
 
 header("Content-type: application/json;charset=utf-8");    
 echo json_encode($data);
