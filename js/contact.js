34f0327f806f4ddebc6b15d9b6a5c66f6470c7b7
Ham1S <roope.ukkonen@gmail.com>
2021-11-11 09:36:33 +0200

    v

--- js/contact.js
+++ js/contact.js
@@ -1,16 +1,13 @@
 // contact.js
 
-
-
-const nimi = document.querySelector('name');
-const sposti = document.querySelector('email');
-const puh = document.querySelector('number');
-const palaute = document.querySelector('feedback');
-
+const nimi = document.querySelector('.name');
+const sposti = document.querySelector('.email');
+const puh = document.querySelector('.number');
+const palaute = document.querySelector('.feedback');
 
 document.forms['palaute'].addEventListener('submit', annaPalaute);
 
 function annaPalaute(event) {
     event.preventDefault();
-    console.log(nimi, sposti, puh, palaute);
+    console.log(nimi.value, sposti.value, puh.value, palaute.value);
 }
