516aa973998a5c3f8187d236dd6a522eaf87f4b3
urhov <urkk2@gmail.com>
2021-11-12 13:00:43 +0200

    yritin saada rekistöröintiä toimimmaan

--- backend/createNewAdmin.php
+++ backend/createNewAdmin.php
@@ -6,13 +6,12 @@ $data = array();
 if (!isset($_POST['username']) || !isset($_POST['password'])){
   $data = array(
     'error' => 'POST-dataa ei saatavilla!'
-    
   );
   die();  
 }
 $username = $_POST['username'];
 $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-$password2 = password_hash($_POST['confirmPassword'], PASSWORD_DEFAULT);
+
 
 include_once 'pdo-connect.php';
 
@@ -21,35 +20,26 @@ try {
     $stmt = $conn->prepare("INSERT INTO users (username, passwd) VALUES (:username, :passwd);");
     $stmt->bindParam(':username', $username);
     $stmt->bindParam(':passwd', $password);
-    $stmt->execute();
-    
-    echo 'tallennettu';
-    
-    
-    
-    
-   //if($stmt->execute() == false){
- //     $data = array(
- //           'error' => 'tapahtui joku virhe tallennuksessa!'
- //       );
- //}  else {
-  // $data = array(
-    //'success' => 'uusi käyttäjä on tallennettu!'
-    //);
-//} 
-  //    }
-//catch (PDOException $e) {
-//if (strpos($e->getMessage(), ' 1062 Duplicate entry')){
-   // $data = array(
-    //   'error' => 'käyttäjä on jo olemassa!'
-    //);
-//} else {
-  //$data = array(
-    //'error' => 'tuli virhe käyttäjän tallentamisessa!'
-    //);  
+    if ($stmt->execute() == false){
+     $data = array(
+       'error' => 'tapahtui joku virhe tallennuksessa'
+     );
+    } else {
+    $data = array(
+-     'success' => 'uusi käyttäjä on tallennettu'
+    );
+  }
+      } catch (PDOException $e) {
+if (strpos($e->getMessage(), ' 1062 Duplicate entry')){
+   $data = array(
+       'error' => 'käyttäjä on jo olemassa!'
+    );
+} else {
+  $data = array(
+    'error' => 'tuli virhe käyttäjän tallentamisessa!'
+    );  
+      } 
+}   
 
-//  } 
-//}    
-//header("Content-type: application/json;charset=utf-8");    
-//echo json_encode($data);
-?>
+header("Content-type: application/json;charset=utf-8");    
+echo json_encode($data);
--- js/register.js
+++ js/register.js
@@ -1,46 +1,51 @@
 //Rekisteröitymissivu
 
-document.forms['register'].addEventListener('submit', loginAdmin);
+document.forms['register'].addEventListener('submit', createNewAdmin);
 
-function loginAdmin(event){
+function createNewAdmin(event){
 
+    event.preventDefault(); 
     
-
     const username = document.forms['register']['username'].value;
     const password = document.forms['register']['password'].value;
     const password2 = document.forms['register']['confirmPassword'].value;
    
     if (username.length <= 0)  { 
-    alert('error', 'username is required');
+    showMessage('error', 'username is required');
     return;
 }
 
 if (password.length <= 4) {
-    alert('error', 'minimum is 4 characters');
+    showMessage('error', 'minimum is 4 characters');
     return;
 }
 
-if (password.localeCompare(password2) != 0 ){
-    alert('error','Password not matching!');
+if (password.localeCompare(password2) != 0){
+    showMessage(data.error);
     return;
 }
+
+console.log('registering new user');
 
     let ajax = new XMLHttpRequest();
     ajax.onload = function(){
     const data = JSON.parse(this.responseText); 
     if (data.hasOwnProperty('success')){
-    window.location.href = "admin.php?type=success&msg=rekisteröityminen onnistui! voit kirjautua uusilla tunnuksilla!";
-    return;
+        alert('tallennus onnistui!');
+    //window.location.href = "admin.php?type=success&msg=rekisteröityminen onnistui! voit kirjautua uusilla tunnuksilla!";
+    //return;
    } else {
-        alert('error' ,data.error);
+        showMessage(data.error);
     }
     ajax.open("POST", "backend/createNewAdmin.php", true);
     ajax.setRequestHeader("content-type", "application/x-www-form-urlencoded");
     ajax.send("username="+username+"&password="+password);
 }
+      }
 
-
-        }
-
-
+function showMessage(msg){
+    let msgBox = document.getElementById("msg");
+    msgBox.querySelector('p').innerHTML = msg;
+    msgBox.classList.remove('d-none');
+}
 
