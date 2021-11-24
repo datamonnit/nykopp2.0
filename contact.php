0034b689da02059894e3049ba468a2053d4c6428
Ham1S <roope.ukkonen@gmail.com>
2021-11-24 09:02:28 +0200

    commit

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@
     <p>Email:</p>
     <input type="text" class="email" name="email" placeholder="email@example.com"></input><br>
     <p>Phone number:</p>
-    <input type="text" class="number" name="number" placeholder="+358 123456789"></input><br>
+    <input type="number" class="number" name="number" placeholder="+358 123456789"></input><br>
     <p>Feedback:</p>
     <textarea type="textarea" class="feedback" name="feedback"></textarea><br>
     <button class="btn" type="submit">Send</button>
