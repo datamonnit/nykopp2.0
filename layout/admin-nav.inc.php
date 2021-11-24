d9ab891f64b98ce069c55d410e03dd4ebe22c3ce
Tuomas Puro <tuomas.puro@esedu.fi>
2021-11-24 09:06:23 +0200

    messagebox admin naviin

--- layout/admin-nav.inc.php
+++ layout/admin-nav.inc.php
@@ -32,3 +32,11 @@
     </div>
   </div>
 </nav>
+
+<div class="container">
+  <div id="msg" class="alert alert-dismissible alert-warning d-none">
+    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+    <h4 class="alert-heading"></h4>
+    <p class="mb-0"></p>
+  </div>
+</div>
