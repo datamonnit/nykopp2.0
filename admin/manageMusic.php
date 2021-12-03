a2fdf480f086541a536913795ecd446153a9cc21
urhov <urkki2@gmail.com>
2021-12-03 14:05:32 +0200

    joo

--- admin/manageMusic.php
+++ admin/manageMusic.php
@@ -2,20 +2,18 @@
 include_once '../layout/top.inc.php';
 include_once '../layout/nav.inc.php';
 ?>
+
+
 <div class="container">
     <div class="row">
         <div class="col">
-            <div id="music">
             <h2>editoi musiikkia</h2>
-                <h4>Telemannic dreaming</h4>
-            </div>
-            <ul id="musicUl" class="list-group">
+            <ul id="musicUl" class="form-group">
+                
             </ul>
         </div>
     </div>
 </div>
-
-
 
 
 
