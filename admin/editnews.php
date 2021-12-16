013e8912fbdc3b3e9762fb0d403dbc41a42def82
Elmeri1 <henry.e.vaisanen@gmail.com>
2021-12-16 15:44:22 +0200

    editNews valikko paremmaks

--- admin/editnews.php
+++ admin/editnews.php
@@ -8,6 +8,8 @@
 <?php include_once '../layout/top.inc.php'; ?>
 <?php include_once '../layout/nav.inc.php'; ?>
 
+<div class="container">
+
 <form name="editNews">
 <fieldset>
     <legend>Muokkaa uutista</legend>
@@ -16,13 +18,21 @@
 
     <input name="id" type="hidden" class="form-control" placeholder="id">
 
+    <div class="mb-3">
     <input name="topic" type="text" class="form-control" placeholder="topic">
+    </div>
 
+    <div class="mb-3">
     <input name="content" type="text" class="form-control" placeholder="content">
+    </div>
 
+    <div class="mb-3">
     <input name="date" type="datetime-local" class="form-control">
+    </div>
 
+    <div class="mb-3">
     <input name="expdate" type="datetime-local" class="form-control">
+    </div>
 
 </fieldset>
 
