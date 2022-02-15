01fbabb9fe35b8012dbe53b87a22fb3363adf8c4
Tuomas Puro <tuomas.puro@esedu.fi>
2022-02-15 11:15:19 +0200

    Jotain pieniä muutoksia

--- layout/top.inc.php
+++ layout/top.inc.php
@@ -9,6 +9,9 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <style>
+      body {
+        /* overflow: hidden; */
+      }
       #imagesUl img {
         object-fit: cover;
         width: 100%;
@@ -29,15 +32,17 @@
 
       .os-container {
         width: 100vw;
-        height: 100vh;
+        height: 94vh;
+        overflow: hidden;
         
       }
       #openseadragon1 {
         
         top: 0;
         left: 0;
-        width:100vw; height: 90vh;
+        width:100%; height: 100%;
         z-index: 1;
+        overflow: hidden;
       }
     </style>
     <title>Lauri Nykopp</title>
